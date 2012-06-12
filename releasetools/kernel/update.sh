9e3a1ee136f3dfe3c46bd1dde2435249ef5fe16f
TamCore <philipp@tamcore.eu>
2012-06-12 14:46:08 +0200

    releasetools: store configuration in /data/local/tamcore-kernel.sh instead in an initscript which gets deleted every flash

--- releasetools/kernel/update.sh
+++ releasetools/kernel/update.sh
@@ -1,14 +1,9 @@
 #!/sbin/sh
-if [ "$1" == "backup" ] && [ -e "/system/etc/init.d/00tamcore-kernel" ];
+if [ -e "/system/etc/init.d/00tamcore-kernel" ];
 then
-	grep -E "^(SWEEP2WAKE|BADASS|TUN|ZRAM)" /system/etc/init.d/00tamcore-kernel > /tmp/kernel.sh
-elif [ "$1" == "restore" ] && [ -e "/system/etc/init.d/00tamcore-kernel" ];
-then
-	source /tmp/kernel.sh
-	sed -i \
-	-e "s#^SWEEP2WAKE=.*#SWEEP2WAKE=${SWEEP2WAKE}#" \
-	-e "s#^BADASS=.*#BADASS=${BADASS}#" \
-	-e "s#^TUN=.*#TUN=${TUN}#" \
-	-e "s#^ZRAM=.*#ZRAM=${ZRAM}#" \
-	/system/etc/init.d/00tamcore-kernel
+	mount /data
+	if [ ! -e "/data/local/tamcore-kernel.sh" ]; then
+		grep -E "^(SWEEP2WAKE|BADASS|TUN|ZRAM)" /system/etc/init.d/00tamcore-kernel > /data/local/tamcore-kernel.sh
+	fi
+	umount /data
 fi
